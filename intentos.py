59f1df837abca801b9288c41573a74b704a79439
Holocron09 <mick_18mex@hotmail.com>
2019-11-14 18:22:42 -0600

    Pruebas a sensor (Lectura)

---
+++ intentos.py
@@ -0,0 +1,9 @@
+import board
+import busio
+import adafruit_bme280
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+print("\nTemperature: %0.1f C" % bme280.temperature)
+print("Humidity: %0.1f %%" % bme280.humidity)
+print("Pressure: %0.1f hPa" % bme280.pressure)
+print("Altitude: %0.2f m" %bme280.altitude)
